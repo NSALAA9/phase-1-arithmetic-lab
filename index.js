09826df60fe86caf7b8a7568ee301701b953ff32
NSALAA9 <alaansairat99@gmail.com>
2023-10-01 14:25:57 +0000

    Completed

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+
+//Q1
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+console.log(multiply);
+
+//Q2
+const random=Math.floor(Math.random() + 1);
+console.log(random);
+//Q3
+const num3 = 14;
+const num4 = 10;
+const mod = (num3 % num4);
+
+console.log(mod);
+
+//Q4
+const max=Math.max(1, 2, 3, 4, 20);
+console.log(max);
